6f5ebda9ba83d988c5a1ed0c8e749df12234e9d5
justgnoh <justingnoh@gmail.com>
2021-11-04 13:40:44 +0800

    Add pool.connect

--- queries.js
+++ queries.js
@@ -9,10 +9,23 @@ const Pool = require('pg').Pool
  
 const pool = new Pool({
   user:'justingnoh',
-  host: '127.0.0.1',
+  host: 'localhost',
   database:'api',
   password: 'justgnoh',
   port:5432
+})
+
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack)
+  }
+  client.query('SELECT NOW()', (err, result) => {
+    release()
+    if (err) {
+      return console.error('Error executing query', err.stack)
+    }
+    console.log(result.rows)
+  })
 })
 
 // GET Users
